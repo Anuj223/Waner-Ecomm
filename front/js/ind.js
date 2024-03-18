c3e81f1d92bdc4ef69097a83737655581e7750c8
Anuj223 <anujgoel790@gmail.com>
2024-03-18 20:32:19 +0530

    Add different number of language option

--- front/js/ind.js
+++ front/js/ind.js
@@ -1,6 +1,12 @@
 
-const  country= document.querySelectorAll(".add-icon select")
-const img= document.querySelector(".lang-img")
+        // const translate = require("google-translate-api")
+        const country= document.querySelectorAll(".add-icon select")
+        const img= document.querySelector(".lang-img")
+        const selectlang = document.querySelectorAll(".nav-lang select")
+        
+        /*
+*For Countries
+*/ 
 for(let select of country){
         for(code in countryList){
                 let newOption = document.createElement("option");
@@ -8,7 +14,7 @@ for(let select of country){
                 newOption.value=code;
                 select.append(newOption);
         }
-
+        
         select.addEventListener("change",(evt)=>{
                 updateflag(evt.target);
         })
@@ -19,5 +25,28 @@ const updateflag=(Element)=>{
         let countrycode = countryList[countryname];
         let newSrc = `https://flagsapi.com/${countrycode}/flat/64.png`
         img.src = newSrc;
+}
+/**
+ * For languages
+*/
+ const updatelang = (Element) =>{
+         let lang = Element.value;
+         let langcode = languages[lang];
+         document.documentElement.lang = langcode;
+         document.body.lang = langcode;
+         //  document.styleSheets.lang = langcode;  
+ }
+for(let select of selectlang)
+{
+        for(lang in languages)
+        {
+                const newlang= document.createElement("option");
+                newlang.innerHTML=lang;
+                newlang.value=lang;
+                select.append(newlang);
+        }
+        select.addEventListener("change",(evt)=>{
+                updatelang(evt.target);
+        })
 }
 
